2cb756b594d3cc0b4c17f3a353075723f4dd1e77
Angad Tendulkar <angad@angad.page>
2025-10-14 16:43:01 -0400

    actions: use toolchain.toml

--- flake.nix
+++ flake.nix
@@ -7,23 +7,50 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
 
-        rustToolchain =
-          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-        libraries = with pkgs; [ wayland libxkbcommon libGL vulkan-loader vulkan-headers gcc.cc.lib ];
-      in {
+        libraries = with pkgs; [
+          wayland
+          libxkbcommon
+          libGL
+          vulkan-loader
+          vulkan-headers
+          gcc.cc.lib
+          pkgsCross.mingwW64.windows.pthreads
+        ];
+      in
+      {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [ rustToolchain pkg-config sccache mold clang ] ++ libraries;
+          buildInputs =
+            with pkgs;
+            [
+              rustToolchain
+              pkg-config
+              sccache
+              mold
+              clang
+              nil
+              pkgsCross.mingwW64.stdenv.cc
+              nixd
+            ]
+            ++ libraries;
           RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libraries;
           PKG_CONFIG_PATH = pkgs.lib.makeSearchPath "lib/pkgconfig" libraries;
         };
-      });
+      }
+    );
 }
