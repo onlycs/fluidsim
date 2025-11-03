b5a7f001a5f433e26e7412dd893ab32f3e4ba7c7
Angad Tendulkar <angad@angad.page>
2025-11-02 22:25:03 -0500

    ?!?! (CI)

--- crates/fluidsim/build.rs
+++ crates/fluidsim/build.rs
@@ -1,14 +1,16 @@
 use spirv_builder::{MetadataPrintout, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = concat!(env!("CARGO_MANIFEST_DIR"), "/../physics");
+
     println!(
-        "cargo:rerun-if-changed={}/../physics/**/*",
-        env!("CARGO_MANIFEST_DIR")
+        "cargo:rerun-if-changed={}/**/*",
+        dir
     );
     println!("cargo:rerun-if-changed=build.rs");
     print!("");
 
-    SpirvBuilder::new("../physics", "spirv-unknown-vulkan1.2")
+    SpirvBuilder::new(dir, "spirv-unknown-vulkan1.2")
         .print_metadata(MetadataPrintout::Full)
         .build()?;
 
